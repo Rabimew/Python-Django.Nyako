de88fa2ff62b0fe7862f88beff5e04ce4eb06011
4Nyako <4Nyako@users.noreply.github.com>
2020-05-13 00:17:09 +0800

    Update test.py

--- tgbot/test.py
+++ tgbot/test.py
@@ -9,23 +9,43 @@ updater = Updater(token='816720263:AAHFSFCQjqIP9yXVUleESaYQRPCGSK04sFU', use_con
 dispatcher = updater.dispatcher
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
-
+cats=0
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="呱呱呱~!")
+    print(f'用户{update.effective_chat.id}发送了{update.message.text},我回复了呱呱呱~!')
 
 def echo(update, context):
     if update.message.text=='你好':
         context.bot.send_message(chat_id=update.effective_chat.id, text=f'你好鸭{update.effective_chat.id},今天吃小猫了吗！')
+        print(f'用户{update.effective_chat.id}发送了{update.message.text},我回复了今天吃小猫了吗！')
     else:
         context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text*2)
+        print(f'用户{update.effective_chat.id}发送了{update.message.text},我回复了{update.message.text*2}')
 
 def caps(update, context):
     text_caps = ' '.join(context.args).upper()
     context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+    print(f'用户{update.effective_chat.id}发送了{update.message.text},我回复了{text_caps}')
 
 def eatcat(update, context):
     text_caps = ' '.join(context.args).upper()
-    context.bot.send_message(chat_id=update.effective_chat.id, text='小猫好吃！！！！！！！！！！！！！！')
+    cats=cats+1
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f'{cats}小猫好吃！！！！！！！！！！！！！！')
+    print(f'用户{update.effective_chat.id}发送了{update.message.text},我回复了{cats}小猫好吃！！！！！！！！！！！！！！')
+
+def inline_eatcat(update, context):
+    query = update.inline_query.query
+    if not query:
+        return
+    results = list()
+    results.append(
+        InlineQueryResultArticle(
+            id=query.upper(),
+            title='eatcat',
+            input_message_content=InputTextMessageContent(query.upper())
+        )
+    )
+    context.bot.answer_inline_query(update.inline_query.id, results)
 
 
 def inline_caps(update, context):
@@ -61,6 +81,9 @@ dispatcher.add_handler(eatcat_handler)
 
 inline_caps_handler = InlineQueryHandler(inline_caps)
 dispatcher.add_handler(inline_caps_handler)
+
+inline_eatcat_handler = InlineQueryHandler(inline_eatcat)
+dispatcher.add_handler(inline_eatcat_handler)
 
 unknown_handler = MessageHandler(Filters.command, unknown)
 dispatcher.add_handler(unknown_handler)
