058f3518cb394f1d345b4d54ddf87a0dc35ffb8b
4Nyako <4Nyako@users.noreply.github.com>
2020-05-17 20:50:41 +0800

    Update tulingTest.py

--- tgbot/tulingTest.py
+++ tgbot/tulingTest.py
@@ -1,18 +1,31 @@
-import os
-import time
-a='现在可以使用telegram的机器人添加日志辣|让机器人在指定目录下塞进去了指定格式的文件XD！！'
-fengefu = (a).index('|')
-biaoti = (a)[0:fengefu - 1]
-neirong = (a)[fengefu + 1:]
-filePath = "../Nyako/rizhis/" + biaoti + "/"
-state = os.path.exists(filePath)  # 判断路径是否存在
-if state:
-    print("File Exist!")
-else:
-    os.makedirs(filePath)  # 创建目录
-    fileName = biaoti + '.txt'
-    f = open(filePath + fileName, 'w', encoding='utf-8')
-    pinglun = open(filePath + 'pinglun.txt', 'w', encoding='utf-8')
-    note = neirong
-    f.write(note)
-    pinglun.write("")
+import requests
+
+
+def bot1(s):
+        resp=requests.get("http://api.qingyunke.com/api.php",{
+            'key':'free',
+            'appid':0,
+            'msg':s
+        })
+        resp.encoding='utf8'
+        resp=resp.json()
+        r=resp['content']
+        print('bot1:'+r)
+        bot2(r)
+        return r
+
+def bot2(s):
+        resp = requests.post("http://www.tuling123.com/openapi/api", data={
+            "key": "d59c41e816154441ace453269ea08dba",
+            "info": s,
+            "userid": "123456"
+        })
+        resp.encoding='utf8'
+        resp=resp.json()
+        r=resp['text']
+        print('bot2:'+r)
+        bot1(r)
+        return r
+
+s=input()
+bot1(s)
